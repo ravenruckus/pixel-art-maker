abf90a19ff206c7a26c823dcce435b833be49cb3
ravenruckus <gyorihome@hotmail.com>
2016-11-12 09:04:48 -0800

    Update pixel art

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+
 (function() {
   'use strict';
   const getCanvas = document.getElementById('canvas')
-  const getColors =document.
+  const getColors =document.getElementById('colors')
+  let currentColor = 'red';
+  const colorArray = ['red', 'blue', 'yellow']
+  let color = 'red'
+  getColors.addEventListener('click', (event) => {
+    if(event.target === getColors) {
+      return;
+    }
+      currentColor = event.target.classList;
+      let colorIndex = colorArray.indexOf(currentColor.value)
+      console.log(currentColor.value)
+      console.log(colorIndex)
+      console.log(colorArray[colorIndex])
+      color = colorArray[colorIndex]
+  })
+// search color array and find index then assign proper color to classlist below.
+
   getCanvas.addEventListener('click', (event) => {
     if(event.target === getCanvas) {
       return;
     }
     // getMain.classList.add('red');
-      event.target.classList.add('red');
+      event.target.classList.add(color);
   })
 })();
+
 
 
 
