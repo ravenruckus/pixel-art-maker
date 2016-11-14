82fbbd50152d764a472cf15078e4452bb5d7f694
ravenruckus <gyorihome@hotmail.com>
2016-11-14 11:13:47 -0800

    Update current color

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
   const newDiv = document.createElement('div')
   const colorGroup = document.createElement('div')
   const reset = document.createElement('div')
+  const heading = document.getElementsByTagName('h1')
+  console.log(heading)
   let currentColor = 'white';
   const colorArray =['rgb(240, 56, 110)','rgb(20, 101, 102)','rgb(96, 171, 153)','rgb(143, 74, 170)','#fff', '#000', 'rgb(89, 193, 117)','rgb(87, 81, 172)','rgb(201, 20, 133)','rgb(125, 72, 53)','rgb(73, 209, 251)', 'rgb(43, 173, 42)','rgb(241, 230, 174)','#7FFFD4','#8A2BE2','rgb(44, 116, 254)','rgb(252, 255, 87)','rgb(44, 26, 115)','rgb(249, 41, 41)','rgb(24, 90, 167)' ]
 
@@ -29,16 +31,20 @@
    colorGroup.setAttribute('id', 'colors')
 
    const getColors =document.getElementById('colors')
-   console.log(getColors)
+  //  console.log(getColors)
    getColors.addEventListener('click', (event) => {
     if(event.target === getColors) {
       return;
     }
-      console.log(event.target.attributes.style.value)
+      // console.log(event.target.attributes.style.value)
       currentColor = event.target.attributes.style.value;
       let colorIndex = colorArray.indexOf(currentColor.value)
+      heading[0].setAttribute('style', currentColor + ' border-radius: 10px; border-bottom: 2px #fff solid')
+
+
   })
 const getCanvas = document.getElementById('canvas')
+//maybe change h1 to currentColor
 
 //   getCanvas.addEventListener('mousedown', (event) => {
 //
@@ -59,11 +65,41 @@ const getCanvas = document.getElementById('canvas')
 })();
 
 
+//
+//
+// let isPainting = false;
+//
+// canvas.addEventListener('mousedown', () => {
+//   isPainting = true;
+//
+//   if (event.target === canvas) {
+//     return;
+//   }
+//   paintPixel(event.target);
+// });
+//
+// document.addEventListenr('mouseup', () => {
+//   isPainting = false;
+// });
+// const pixel = document.querySelectorAll('.pixel');
+//
+// for(const pixel of pixels) {
+//   pixel.addEventLister('mouseenter', ()=>
+//   if (isPainting) {
+//     paintPixel(event.target);
+//   }
+// )
+// }
+//
+// wet variable false
+//  mousedown true paint pixel
+//  mouseup false
+//  select all pixesl
+//
+//  add even lister mouseenter if is painting is true
 
 
-
-
-
+//create an element, add a class, append a child
 
 
 
